5f8e83fca67dc1188011a58d33c73ec7636e38b2
uoftcompeng <46634221+uoftcompeng@users.noreply.github.com>
2019-01-12 17:25:26 -0500

    Update evaluate.py
    Making changes for comments.

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,7 @@
 '''
 Created on 10 lut 2018
 
-@author: mgdak
+Run Evaluation % on final evaulatino of the model for accuracy.
 '''
 import warnings
 warnings.filterwarnings("ignore")
@@ -30,55 +30,32 @@ def load_hist_model_w (model_name):
 
 
 
-    
+#Basic configuration settings.
 img_width, img_height = 224, 224
 batch_size = 16
-test_path = '/Users/dk_macpro/Documents/cars/val_dk/'
+test_path = '/Users/dk_macpro/Documents/cars/val/'
+#Using the model VGG16_SGD_Unlocked_NoReg_NoInit, LR 0.0003
 model_name = "VGG16_SGD_UnLocked_NoReg_NoInit2_lr"
 learn_rate = 0.0003
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
-
 
 test_generator = test_datagen.flow_from_directory(test_path,
                                                    target_size=(img_height, img_width),
                                                    batch_size=batch_size,
                                                    class_mode='categorical')
 
-
 filenames = test_generator.filenames
 num_samples = len(filenames)
 
 test_generator.reset()
 
-
+#Loading Pretrained model
 loaded_model= load_hist_model_w(model_name)
-
+#Choose SGD, compile the loaded model wit hSGD
 sgd = SGD(lr=learn_rate, decay=1e-4, momentum=0.9, nesterov=True)
 loaded_model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
-
-#score = loaded_model.predict_generator(test_generator, verbose=1,steps=num_samples/batch_size)
-#predicted_class_indices=np.argmax(score,axis=1)
-
+#Run Evaluate Generator to get the score and present score in %
 score = loaded_model.evaluate_generator(test_generator,verbose=1, steps=num_samples)
 print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
-
-##SKIP FOR NOW Labelling 
-#labels = (test_generator.class_indices)
-#labels = dict((v,k) for k,v in labels.items())
-#predictions = [labels[k] for k in predicted_class_indices]
-#
-#
-#filenames=test_generator.filenames
-#results=pd.DataFrame({"Filename":filenames,
-#                      "Predictions":predictions})
-    
-    
-#for i in range(0, num_samples):
-#    classidx = np.argmax(score[i])
-#    print ("Prediction Class: ",classidx , " , Accuracy: %.2f" % round(score[i][classidx]*100,2),"%")
-
-
-
-
